8ec9e1c106a2b2ad2c79caadfc915343acaa6164
Muhammed Afsal <afsal983@gmail.com>
2022-02-13 13:44:23 +0400

    first commit

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~> 3.27"
+        }
+    }
+    required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region = "ap-south-1"
+}
+
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+  bucket = "salprds3bucket"
+  acl = "private"
+
+  versioning = {
+    enabled = true
+  }
+
+  
+}
