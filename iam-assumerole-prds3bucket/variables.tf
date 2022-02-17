2fce40aaae744618017383540b9be4eeba283c0b
Muhammed Afsal <afsal983@gmail.com>
2022-02-17 19:09:44 +0400

    policy doc2

--- iam-assumerole-prds3bucket/variables.tf
+++ iam-assumerole-prds3bucket/variables.tf
@@ -23,7 +23,7 @@ data "aws_iam_policy_document" "bucket_policy" {
 
     resources = [
       module.s3_bucket.s3_bucket_arn,
-      "${module.s3_bucket.s3_bucket_arn}/${aws.username}/*",
+      "${module.s3_bucket.s3_bucket_arn}/*",
     ]
   }
 
